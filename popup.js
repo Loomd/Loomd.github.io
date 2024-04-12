4888119827bd0488f0b62a001ec403776f322e8d
Aaron Hunter <122066290+Loomd@users.noreply.github.com>
2024-04-12 18:07:55 -0500

    Create popup.js
    goofin

---
+++ popup.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+    alert(`Well looky here, why'd you decide to come here? Don't you have better things to do with your life... 
+    Anyways welcome I guess. Kick your shoes off and stay a while might as well make it worth the visit.`);
+};
