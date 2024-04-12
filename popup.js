25e20c8752cb2bf313ab861b572c7deee6bd3636
Aaron Hunter <122066290+Loomd@users.noreply.github.com>
2024-04-12 18:15:15 -0500

    Update popup.js
    extrapolate

--- popup.js
+++ popup.js
@@ -1,4 +1,10 @@
 window.onload = function() {
-    alert(`Well looky here, why'd you decide to come here? Don't you have better things to do with your life... 
-    Anyways welcome I guess. Kick your shoes off and stay a while might as well make it worth the visit.`);
+    // Check if the pop-up has already been shown
+    if (!localStorage.getItem('popupShown')) {
+        alert(`Well looky here, why'd you decide to come here? Don't you have better things to do with your life... Anyways welcome I guess. 
+Kick your shoes off and stay a while might as well make it worth the visit.`);
+
+        // Set 'popupShown' in localStorage
+        localStorage.setItem('popupShown', 'true');
+    }
 };
