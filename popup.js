34bf92a6facbbbb21022bcb24eaff81c7e4525d1
Aaron Hunter <122066290+Loomd@users.noreply.github.com>
2024-05-07 13:01:07 -0500

    Delete popup.js

--- popup.js
+++
@@ -1,4 +0,0 @@
-window.onload = function() {
-    alert(`Well looky here, why'd you decide to come here? Don't you have better things to do with your life... Anyways welcome I guess. 
-Kick your shoes off and stay a while might as well make it worth the visit.`);
-};
